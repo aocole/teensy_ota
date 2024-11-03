b9d303942f59b46800b72216d708787dcbbac2ff
Andrew Cole <aocole@gmail.com>
2024-11-03 12:16:35 -0800

    Remove serial port dependency

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # teensy_ota
-Teensy OTA update test code
+Teensy OTA update via Ethernet POC code. 
+
+On boot the teensy will join the network via DHCP and print its address to the serial port. Static IP config available.
+Visit the IP address via a web browser to see the firmare `.hex` file upload form. This builds on work from @ssaenger/FlasherX-Ethernet_Support
+(who did a much better job encapsulating the code). However I couldn't get that code to work reliably - it would hang almost every time while
+trying to reboot after copying the new firmware. Writing the new file to the SD card and then using FlasherX to copy from the SD card was 100%
+reliable. This required modifying FlasherX to skip interactive confirmation of the update.
+
+## URLs:
+
+ - `GET /` - displays upload form
+ - `POST /` - upload new firmware `.hex` file as multipart/form-data
+
+## Required libraries
+
+ - QNEthernet
+ - AsyncWebServer_Teensy41 v1.7.0+
--- teensy_ota.ino
+++ teensy_ota.ino
@@ -54,9 +54,9 @@
 //    https://namoseley.wordpress.com/2015/02/04/freescale-kinetis-mk20dx-series-flash-erasing/
 
 #include <SD.h>
-#include "src/FlasherX/FXUtil.h"		// read_ascii_line(), hex file support
+#include "src/FlasherX/FXUtil.h"  // read_ascii_line(), hex file support
 extern "C" {
-  #include "src/FlasherX/FlashTxx.h"		// TLC/T3x/T4x/TMM flash primitives
+#include "src/FlasherX/FlashTxx.h"  // TLC/T3x/T4x/TMM flash primitives
 }
 #include <QNEthernet.h>
 #include <AsyncWebServer_Teensy41.h>
@@ -64,7 +64,7 @@ extern "C" {
 using namespace qindesign::network;
 
 #define WAIT_FOR_LOCAL_IP_WAIT_TIME 15000
-#define HOSTNAME "BAJ"
+#define HOSTNAME "baj"
 #define STATIC_IP_ADDR_STRNG "192.168.1.113"
 #define GATEWAY_IP_ADDR_STRING "192.168.1.1"
 #define SUBNET_MASK_STRING "255.255.255.0"
@@ -75,247 +75,214 @@ using namespace qindesign::network;
 
 #define HTTP_MAX_MESSAGE_RESP 512
 
-#define VERSION 1
+// This number is printed later just to demonstrate that the firmware
+// version has changed.
+#define VERSION 2
 
 #if !USING_DHCP
-  // Set the static IP address to use if the DHCP fails to assign
-  IPAddress myIP;
-  IPAddress myNetmask;
-  IPAddress myGW;
-  IPAddress mydnsServer;
+// Set the static IP address to use if the DHCP fails to assign
+IPAddress myIP;
+IPAddress myNetmask;
+IPAddress myGW;
+IPAddress mydnsServer;
 #endif
 
-AsyncWebServer   *webServer;
-bool              updateAvailable = false;
+AsyncWebServer *webServer;
+bool updateAvailable = false;
 
-void handleNotFound(AsyncWebServerRequest *Request)
-{
-    String message = "File Not Found\n\n";
-    uint8_t i;
+void handleNotFound(AsyncWebServerRequest *Request) {
+  String message = "File Not Found\n\n";
+  uint8_t i;
 
-    message += "URI: ";
-    message += Request->url();
-    message += "\nMethod: ";
-    message += (Request->method() == HTTP_GET) ? "GET" : "POST";
-    message += "\nArguments: ";
-    message += Request->args();
-    message += "\n";
+  message += "URI: ";
+  message += Request->url();
+  message += "\nMethod: ";
+  message += (Request->method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += Request->args();
+  message += "\n";
 
-    for (i = 0; i < Request->args(); i++) {
-        message += " " + Request->argName(i) + ": " + Request->arg(i) + "\n";
-    }
+  for (i = 0; i < Request->args(); i++) {
+    message += " " + Request->argName(i) + ": " + Request->arg(i) + "\n";
+  }
 
-    Request->send(404, "text/plain", message);
+  Request->send(404, "text/plain", message);
 }
 
 
-const int cs = BUILTIN_SDCARD;	// SD chip select pin
-const int led = LED_BUILTIN;	// LED pin
-Stream *serial = &Serial;	// Serial (USB) or Serial1, Serial2, etc. (UART)
+const int cs = BUILTIN_SDCARD;   // SD chip select pin
+const int ledPin = LED_BUILTIN;  // LED pin
+Stream *serial = &Serial;        // Serial (USB) or Serial1, Serial2, etc. (UART)
 
 #define FLASHERX_VERSION "FlasherX v2.3"
-#define HEX_FILE_NAME "ota_update.hex"	
+#define HEX_FILE_NAME "ota_update.hex"
 
-#define LARGE_ARRAY (0)		// 1 = define large array to test large hex file
-
-#if (LARGE_ARRAY)
-// nested arrays of integers to add code size for testing 
-#define A0 { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15}  // 16  elements 64
-#define A1 {A0,A0,A0,A0,A0,A0,A0,A0,A0,A0,A0,A0,A0,A0,A0,A0}  // 256 elements 1KB 
-#define A2 {A1,A1,A1,A1,A1,A1,A1,A1,A1,A1,A1,A1,A1,A1,A1,A1}  // 4K  elements 16KB
-#define A3 {A2,A2,A2,A2,A2,A2,A2,A2,A2,A2,A2,A2,A2,A2,A2,A2}  // 64K elements 256KB 
-#define A4 {A3,A3,A3,A3,A3,A3,A3,A3,A3,A3,A3,A3,A3,A3,A3,A3}  // 1M  elements 4MB
-
-// const variables reside in flash and get optimized out if never accessed
-// use uint8_t -> 1MB, uint16_t -> 2MB, uint32_t -> 4MB, uint64_t -> 8MB)
-PROGMEM const uint8_t a[16][16][16][16][16] = A4;
-#endif
-
-const int ledPin = 13;
-const char* urlPath = "/";
+const char *urlPath = "/";
 File hexFile;
 
 void update_from_sdcard(uint32_t buffer_addr, uint32_t buffer_size) {
-    if (!SD.begin( cs )) {
-      serial->println( "SD initialization failed" );
+  if (!SD.begin(cs)) {
+    serial->println("SD initialization failed");
+    return;
+  }
+  File hexFile;
+  serial->println("SD initialization OK");
+  hexFile = SD.open(HEX_FILE_NAME, FILE_READ);
+  if (!hexFile) {
+    serial->println("SD file open failed");
+    return;
+  }
+  serial->println("SD file open OK");
+  // read hex file, write new firmware to flash, clean up, reboot
+  update_firmware(&hexFile, serial, buffer_addr, buffer_size);
+}
+
+void EndOta(AsyncWebServerRequest *request) {
+  if (hexFile) {
+    hexFile.close();
+    Serial.println("closing hex file");
+  }
+  AsyncWebParameter *p = request->getParam(0);
+  Serial.printf("FILE[%s]: %s, size: %u\n", p->name().c_str(), p->value().c_str(), p->size());
+
+  char pageOut[HTTP_MAX_MESSAGE_RESP];
+
+  updateAvailable = true;
+  snprintf(pageOut, HTTP_MAX_MESSAGE_RESP, "<body><h1>cool, got it. %u bytes</h1>", p->size());
+  request->send(200, "text/html", pageOut);
+}
+
+void StartOta(AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final) {
+  // Serial.printf("OTA: len, %d, index: %d\r\n", filename, len, index);
+
+  if (!hexFile) {
+    if (!SD.begin(cs)) {
+      serial->println("SD initialization failed");
       return;
     }
-    File hexFile;
-    serial->println( "SD initialization OK" );
-    hexFile = SD.open( HEX_FILE_NAME, FILE_READ );
-    if (!hexFile) {
-      serial->println( "SD file open failed" );
-      return;
+    serial->println("SD initialization OK");
+
+    if (SD.exists(HEX_FILE_NAME)) {
+      serial->println("Update file already exists - deleting...");
+      SD.remove(HEX_FILE_NAME);
     }
-    serial->println( "SD file open OK" );
-    // read hex file, write new firmware to flash, clean up, reboot
-    update_firmware( &hexFile, serial, buffer_addr, buffer_size );
+
+    serial->println("Opening hex file");
+    hexFile = SD.open(HEX_FILE_NAME, FILE_WRITE);
+  }
+  if (hexFile) {
+    hexFile.write(data, len);
+    serial->print(".");
+  } else {
+    serial->println("Couldn't open file to write to");
+    return;
+  }
 }
 
-void EndOta(AsyncWebServerRequest *request)
-{
-    if (hexFile) {
-      hexFile.close();
-      Serial.println("closing hex file");
-    }
-    AsyncWebParameter *p = request->getParam(0);
-    Serial.printf("FILE[%s]: %s, size: %u\n", p->name().c_str(), p->value().c_str(), p->size());
+void DisplayUpdatePage(AsyncWebServerRequest *Request) {
+  Serial.println("GET /");
 
-    char pageOut[HTTP_MAX_MESSAGE_RESP];
-    unsigned int len;
+  char pageOut[HTTP_MAX_MESSAGE_RESP];
+  unsigned int len;
 
-    updateAvailable = true;
-    len = snprintf(pageOut, HTTP_MAX_MESSAGE_RESP, "<body><h1>cool, got it. %u bytes</h1>", p->size());
-    request->send(200, "text/html", pageOut);
-}
+  len = snprintf(pageOut, HTTP_MAX_MESSAGE_RESP, "<body><h1>Ethernet OTA update for Teensy4.1 VERSION: %d</h1>", VERSION);
+  len += snprintf(pageOut + len, HTTP_MAX_MESSAGE_RESP - len, "<br><h2>Select and send your .hex firmware file:</h2><br>");
+  len += snprintf(pageOut + len, HTTP_MAX_MESSAGE_RESP - len, "<div><form method='POST' enctype='multipart/form-data' action='%s'>", urlPath);
+  len += snprintf(pageOut + len, HTTP_MAX_MESSAGE_RESP - len, "<input type='file' name='file'><button type='submit'>Send</button></form></div></body>");
 
-void StartOta(AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final)
-{
-    // int          lineLength;
-    // int          errorVal;
-    // unsigned int off = 0;
-    // char *abortMsg = (char*)"";
-
-    // Serial.printf("OTA: len, %d, index: %d\r\n", filename, len, index);
-    
-    if (!hexFile) {
-      if (!SD.begin( cs )) {
-        serial->println( "SD initialization failed" );
-        return;
-      }
-      serial->println( "SD initialization OK" );
-
-      if (SD.exists(HEX_FILE_NAME)) {
-        serial->println("Update file already exists - deleting...");
-        SD.remove(HEX_FILE_NAME);
-      }
-
-      serial->println( "Opening hex file" );
-      hexFile = SD.open( HEX_FILE_NAME, FILE_WRITE );
-    }
-    if (hexFile) {
-      hexFile.write(data, len);
-      serial->print(".");
-    }
-}
-
-void DisplayUpdatePage(AsyncWebServerRequest *Request)
-{        
-    Serial.println("GET /");
-
-    char pageOut[HTTP_MAX_MESSAGE_RESP];
-    unsigned int len;
-
-    len = snprintf(pageOut, HTTP_MAX_MESSAGE_RESP, "<body><h1>Ethernet OTA update for Teensy4.1</h1>");
-    len += snprintf(pageOut + len, HTTP_MAX_MESSAGE_RESP - len, "<br><h2>Select and send your .hex firmware file:</h2><br>");
-    len += snprintf(pageOut + len, HTTP_MAX_MESSAGE_RESP - len, "<div><form method='POST' enctype='multipart/form-data' action='%s'>", urlPath);
-    len += snprintf(pageOut + len, HTTP_MAX_MESSAGE_RESP - len, "<input type='file' name='file'><button type='submit'>Send</button></form></div></body>");
-
-    Request->send(200, "text/html", pageOut);
+  Request->send(200, "text/html", pageOut);
 }
 
 ////////////////////////// setup() ////////////////////////////////
-void setup () 
-{
+void setup() {
   pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);  // set the LED on
+  delay(200);                  // delay
+  digitalWrite(ledPin, LOW);   // set the LED off
 
-  if (serial == (Stream*)&Serial) {
+  if (serial == (Stream *)&Serial) {
     Serial.begin(115200);
-    while (!Serial) {} 
+  } else {
+    ((HardwareSerial *)serial)->begin(115200);
   }
-  else {
-    ((HardwareSerial*)serial)->begin( 115200 );
-  } 
-  
-  pinMode( led, OUTPUT );	// assign output
-  digitalWrite( led, HIGH );	// set the LED on
-  delay(200);			// delay
-  digitalWrite( led, LOW );	// set the LED off
 
-  serial->printf( "%s - %s %s\n", FLASHERX_VERSION, __DATE__, __TIME__ );
-  serial->printf( "WARNING: this can ruin your device!\n" );
-  serial->printf( "target = %s (%dK flash in %dK sectors)\n",
-			FLASH_ID, FLASH_SIZE/1024, FLASH_SECTOR_SIZE/1024);
-			
-#if (LARGE_ARRAY) // if true, access array so it doesn't get optimized out
-  serial->printf( "Large Array -- %08lX\n", (uint32_t)&a[15][15][15][15][15] );
+  serial->printf("%s - %s %s\n", FLASHERX_VERSION, __DATE__, __TIME__);
+  serial->printf("WARNING: this can ruin your device!\n");
+  serial->printf("target = %s (%dK flash in %dK sectors)\n",
+                 FLASH_ID, FLASH_SIZE / 1024, FLASH_SECTOR_SIZE / 1024);
+
+#if (LARGE_ARRAY)  // if true, access array so it doesn't get optimized out
+  serial->printf("Large Array -- %08lX\n", (uint32_t)&a[15][15][15][15][15]);
 #endif
 
-    Serial.printf("Setting hostname to %s\r\n", HOSTNAME);
-
-    webServer = new AsyncWebServer(80);
+  webServer = new AsyncWebServer(80);
 
 #if LWIP_NETIF_HOSTNAME
-    Serial.printf("Setting hostname to %s\r\n", HOSTNAME);
-    Ethernet.setHostname(HOSTNAME);
+  Serial.printf("Setting hostname to %s\r\n", HOSTNAME);
+  Ethernet.setHostname(HOSTNAME);
 #endif
 
-    // Attempt to establish a connection with the network
+  // Attempt to establish a connection with the network
 #if USING_DHCP
-    // Start the Ethernet connection, using DHCP
-    Serial.print("Initialize Ethernet using DHCP => ");
-    Ethernet.begin();
+  // Start the Ethernet connection, using DHCP
+  Serial.print("Initialize Ethernet using DHCP => ");
+  Ethernet.begin();
 #else
-    // Start the Ethernet connection, using static IP
-    Serial.print("Initialize Ethernet using static IP => ");
-    myIP.fromString(STATIC_IP_ADDR_STRNG);
-    myGW.fromString(GATEWAY_IP_ADDR_STRING);
-    myNetmask.fromString(SUBNET_MASK_STRING);
-    mydnsServer.fromString(DNS_SERVER_STRING);
-    Ethernet.begin(myIP, myNetmask, myGW);
-    Ethernet.setDNSServerIP(mydnsServer);
+  // Start the Ethernet connection, using static IP
+  Serial.print("Initialize Ethernet using static IP => ");
+  myIP.fromString(STATIC_IP_ADDR_STRNG);
+  myGW.fromString(GATEWAY_IP_ADDR_STRING);
+  myNetmask.fromString(SUBNET_MASK_STRING);
+  mydnsServer.fromString(DNS_SERVER_STRING);
+  Ethernet.begin(myIP, myNetmask, myGW);
+  Ethernet.setDNSServerIP(mydnsServer);
 #endif
 
-    if (!Ethernet.waitForLocalIP(WAIT_FOR_LOCAL_IP_WAIT_TIME)) {
-        Serial.println(F("Failed to configure Ethernet"));
+  if (!Ethernet.waitForLocalIP(WAIT_FOR_LOCAL_IP_WAIT_TIME)) {
+    Serial.println(F("Failed to configure Ethernet"));
 
-        if (!Ethernet.linkStatus()) {
-            Serial.println(F("Ethernet cable is not connected."));
-        }
-
-    } else {
-        Serial.print(F("Connected! IP address:"));
-        Serial.println(Ethernet.localIP());
+    if (!Ethernet.linkStatus()) {
+      Serial.println(F("Ethernet cable is not connected."));
     }
 
-    webServer->onNotFound(handleNotFound);
+  } else {
+    Serial.print(F("Connected! IP address:"));
+    Serial.println(Ethernet.localIP());
+  }
 
-    webServer->on(urlPath, HTTP_GET, [&](AsyncWebServerRequest *request)
-        {
-            DisplayUpdatePage(request);
-        });
+  webServer->onNotFound(handleNotFound);
 
-    webServer->on(
-        urlPath, HTTP_POST, [&](AsyncWebServerRequest *request)
-        {
-            EndOta(request);
-        },
-        [&](AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final)
-        {
-            StartOta(request, filename, index, data, len, final);
-        });
-    webServer->begin();
-    
-    Serial.println(F("Done with setup"));
+  webServer->on(urlPath, HTTP_GET, [&](AsyncWebServerRequest *request) {
+    DisplayUpdatePage(request);
+  });
 
+  webServer->on(
+    urlPath, HTTP_POST, [&](AsyncWebServerRequest *request) {
+      EndOta(request);
+    },
+    [&](AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final) {
+      StartOta(request, filename, index, data, len, final);
+    });
+  webServer->begin();
+
+  Serial.println(F("Done with setup"));
 }
 
-void loop ()
-{
+void loop() {
   uint32_t buffer_addr, buffer_size;
 
-  serial->printf( "\n\n Version %d\n", VERSION);
+  serial->printf("\n\n Version %d\n", VERSION);
 
   // create flash buffer to hold new firmware
-  if (firmware_buffer_init( &buffer_addr, &buffer_size ) == 0) {
-    serial->printf( "unable to create buffer\n" );
+  if (firmware_buffer_init(&buffer_addr, &buffer_size) == 0) {
+    serial->printf("unable to create buffer\n");
     serial->flush();
     for (;;) {}
   }
-  
-  serial->printf( "created buffer = %1luK %s (%08lX - %08lX)\n",
-		buffer_size/1024, IN_FLASH(buffer_addr) ? "FLASH" : "RAM",
-		buffer_addr, buffer_addr + buffer_size );
+
+  serial->printf("created buffer = %1luK %s (%08lX - %08lX)\n",
+                 buffer_size / 1024, IN_FLASH(buffer_addr) ? "FLASH" : "RAM",
+                 buffer_addr, buffer_addr + buffer_size);
 
   while (!updateAvailable) {
     delay(200);
@@ -326,8 +293,8 @@ void loop ()
 
   // return from update_firmware() means error or user abort, so clean up and
   // reboot to ensure that static vars get boot-up initialized before retry
-  serial->printf( "erase FLASH buffer / free RAM buffer...\n" );
-  firmware_buffer_free( buffer_addr, buffer_size );
+  serial->printf("Update failed. Erase FLASH buffer / free RAM buffer...\n");
+  firmware_buffer_free(buffer_addr, buffer_size);
   serial->flush();
   REBOOT;
 }
